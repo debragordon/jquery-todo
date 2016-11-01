6455820215e37e3776cf0be75f32eec420dd9ce8
Debra Gordon <debra.m.gordon@gmail.com>
2016-11-01 17:38:42 -0500

    updating repo from alst night

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,105 +1,117 @@
-"use strict";
+// "use strict";
 
-console.log("test in js connection");
+// console.log("test in js connection");
 
-/////// grab user input text
-let $needToDo = $("#need-to-do");
-let $addToDoListMain = $("#add-task-main");
-let toDoListMain = [];
-let toDoListDeleted = [];
-let toDoListCompleted = [];
-let $mainToDoListDOM = $("#todo-list-main-div");
-let $completedTasks = $("#todo-list-done");
+// /////// grab user input text
+// let $needToDo = $("#need-to-do");
+// let $addToDoListMain = $("#add-task-main");
+// let toDoListMain = [];
+// let $mainToDoListDOM = $("#todo-list-main-div");
+// let $completedTasks = $("#todo-list-done");
+// let uniqueId = 0;
 
-//// get val from input field on add button click event
-$addToDoListMain.on("click", function() {
-  console.log("test this click");
-//// add input text to todo list array
-  toDoListMain.push($needToDo.val());
-  console.log("toDoListMain", toDoListMain);
-//// display that line item in the dom with an edit, delete, complete button
-  toDoListMaker();
-});
+// //// get val from input field on add button click event
+// $addToDoListMain.on("click", function() {
+//   console.log("test this click");
+// //// add input text to todo list array
+//   var userText = $needToDo.val();
+//   console.log("toDoListMain", toDoListMain);
+// //// display that line item in the dom with an edit, delete, complete button
+//   createObject(userText);
+// });
 
-function toDoListMaker() {
-  console.log("testing function");
-  $mainToDoListDOM.html("");
-  $mainToDoListDOM.html(`<h3 class="text-center">Things to Do</h3>`);
-  for (var i = 0; i < toDoListMain.length; i++) {
-    let newTask = $(`<div id='task-no-${i}'>
-      <p id='task-${i}' class='task'>${toDoListMain[i]}</p>
-      <button type='button' class='btn btn-default btn-xs editTodo' id='edit-button-${i}'>edit</button>
-      <button type='button' class='btn btn-default btn-xs deleteTodo' id='delete-button-${i}'>delete</button>
-      <button type='button' class='btn btn-default btn-xs completeTodo' id='complete-button-${i}'>complete</button>
-      <button type='button' class='btn btn-default btn-xs restoreTodo hidden' id='complete-button-${i}'>restore</button>
-      </div>`);
-  $mainToDoListDOM.append(newTask);
-  $needToDo.val("");
-  }
-}
+// function createObject (userInput) {
+//   uniqueId++;
+//   var userObject = {
+//     "userTextToDo": userInput,
+//     "id": "task-no-" + (uniqueId-1)
+//   };
+//   toDoListMain.push(userObject);
+//   toDoListMaker();
+// }
 
-/////// edit button in todo list
+// function toDoListMaker() {
+//   console.log("testing function");
+//   $mainToDoListDOM.html("");
+//   $mainToDoListDOM.html(`<h3 class="text-center">Things to Do</h3>`);
+//   for (var i = 0; i < toDoListMain.length; i++) {
+//     let newTask = $(`<div id='task-no-${i}'>
+//       <p id='task-${i}' class='task'>${toDoListMain[i].userTextToDo}</p>
+//       <button type='button' class='btn btn-default btn-xs editTodo' id='edit-button-${i}'>edit</button>
+//       <button type='button' class='btn btn-default btn-xs deleteTodo' id='delete-button-${i}'>delete</button>
+//       <button type='button' class='btn btn-default btn-xs completeTodo' id='complete-button-${i}'>complete</button>
+//       <button type='button' class='btn btn-default btn-xs restoreTodo hidden' id='complete-button-${i}'>restore</button>
+//       </div>`);
+//   $mainToDoListDOM.append(newTask);
+//   $needToDo.val("");
+//   }
+// }
 
-//// takes string and makes it live editable text
-$(document).on("click", ".editTodo", function() {
-  console.log("testing click", $(this));
-  let pTag = $(this).siblings("p");
-  let textToFix = pTag.text();
-  console.log("texttofix", textToFix);
-  let newText = window.prompt("Change Your Task", textToFix);
-  console.log("newText", newText);
-  if (newText !== null) {
-    pTag.text(newText);
-  }
-});
+// /////// edit button in todo list
 
-////// delete button in todo list
-//// deletes the item (and all its buttons) from the dom
-$(document).on("click", ".deleteTodo", function() {
-  console.log("testing click", $(this));
-  let divDelete = $(this).parent("div");
-  console.log("divDelete", divDelete);
-  divDelete.remove();
+// //// takes string and makes it live editable text
+// $(document).on("click", ".editTodo", function() {
+//   console.log("testing click", $(this));
+//   let pTag = $(this).siblings("p");
+//   let textToFix = pTag.text();
+//   console.log("texttofix", textToFix);
+//   let newText = window.prompt("Change Your Task", textToFix);
+//   console.log("newText", newText);
+//   if (newText !== null) {
+//     pTag.text(newText);
+//   }
+// });
 
-  ///////// remove from toDoListMain array /////////
-  ///////// add to toDoListDeleted array /////////
+// ////// delete button in todo list
+// //// deletes the item (and all its buttons) from the dom
+// $(document).on("click", ".deleteTodo", function() {
+//   console.log("testing click", $(this));
+//   let divDelete = $(this).parent("div");
+//   console.log("divDelete", divDelete);
+//   divDelete.remove();
+//   ///////// remove from toDoListMain array /////////
+//   for (var i = 0; i < toDoListMain.length; i++) {
+//     if (toDoListMain[i].id === divDelete.attr("id")) {
+//       var index = toDoListMain.indexOf(toDoListMain[i]);
+//       toDoListMain.splice(index, 1);
+//       console.log("new array", toDoListMain);
+//     }
+//   }
+// });
 
-});
+// ////// complete button in todo list
+// //// removes from the todo array
+// //// adds to the completed array
+// //// displays in the completed section along with a delete button and a restore button
+// $(document).on("click", ".completeTodo", function() {
+//   console.log("testing click", $(this));
+//   let divMove = $(this).parent("div");
+//   console.log("divMove", divMove);
+//   $completedTasks.append(divMove);
+//   for (var i = 0; i < toDoListMain.length; i++) {
+//     if (toDoListMain[i].id === divMove.attr("id")) {
+//       var index = toDoListMain.indexOf(toDoListMain[i]);
+//       toDoListMain.splice(index, 1);
+//       console.log("new array", toDoListMain);
+//     }
+//   }
+//   divMove.children(".editTodo").addClass("hidden");
+//   divMove.children(".deleteTodo").addClass("hidden");
+//   divMove.children(".completeTodo").addClass("hidden");
+//   divMove.children(".restoreTodo").removeClass("hidden");
+// });
 
-////// complete button in todo list
-//// removes from the todo array
-//// adds to the completed array
-//// displays in the completed section along with a delete button and a restore button
-$(document).on("click", ".completeTodo", function() {
-  console.log("testing click", $(this));
-  let divMove = $(this).parent("div");
-  console.log("divMove", divMove);
-  $completedTasks.append(divMove);
-
-  ///////// remove from toDoListMain array /////////
-  ///////// add to toDoListComplete array /////////
-
-  divMove.children(".editTodo").addClass("hidden");
-  divMove.children(".deleteTodo").addClass("hidden");
-  divMove.children(".completeTodo").addClass("hidden");
-  divMove.children(".restoreTodo").removeClass("hidden");
-});
-
-////// restore button in the compeleted list
-//// removes from the completed array
-//// adds text to todo list array
-//// display that line item in the dom with an edit, delete, complete button
-$(document).on("click", ".restoreTodo", function() {
-  console.log("testing click", $(this));
-  let divRestore = $(this).parent("div");
-  console.log("divRestore", divRestore);
-  $mainToDoListDOM.append(divRestore);
-
-  ///////// remove from toDoListMain array /////////
-  ///////// add to toDoListComplete array //////////
-
-  divRestore.children(".editTodo").removeClass("hidden");
-  divRestore.children(".deleteTodo").removeClass("hidden");
-  divRestore.children(".completeTodo").removeClass("hidden");
-  divRestore.children(".restoreTodo").addClass("hidden");
-});
+// ////// restore button in the compeleted list
+// //// removes from the completed array
+// //// adds text to todo list array
+// //// display that line item in the dom with an edit, delete, complete button
+// $(document).on("click", ".restoreTodo", function() {
+//   console.log("testing click", $(this));
+//   let divRestore = $(this).parent("div");
+//   console.log("divRestore", divRestore);
+//   $mainToDoListDOM.append(divRestore);
+//   divRestore.children(".editTodo").removeClass("hidden");
+//   divRestore.children(".deleteTodo").removeClass("hidden");
+//   divRestore.children(".completeTodo").removeClass("hidden");
+//   divRestore.children(".restoreTodo").addClass("hidden");
+// });
